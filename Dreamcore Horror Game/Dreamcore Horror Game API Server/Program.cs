193745853d23d043a8b5d887791539c6a48c639d
KroN136 <kron2002a@yandex.ru>
2024-03-06 16:35:33 +0300

    API: Switched to .NET 6

--- Dreamcore Horror Game/Dreamcore Horror Game API Server/Program.cs
+++ Dreamcore Horror Game/Dreamcore Horror Game API Server/Program.cs
@@ -1,4 +1,3 @@
-
 namespace Dreamcore_Horror_Game_API_Server
 {
     public class Program
---
+++ Dreamcore Horror Game/Dreamcore Horror Game API Server/TestClass.cs
@@ -0,0 +1,8 @@
+﻿namespace Dreamcore_Horror_Game_API_Server
+{
+    public class TestClass
+    {
+        private string connectionString = "Driver={PostgreSQL ODBC Driver(UNICODE)}; Server=localhost;Port=5432;Database=dreamcore_horror_game;UID=postgres;PWD=root";
+        private string scaffoldCommand = "Scaffold-DbContext \"Host=localhost;Port=5432;Database=dreamcore_horror_game;Username=postgres;Password=root\" (Provider: Npgsql.EntityFrameworkCore.PostgreSQL)";
+    }
+}
