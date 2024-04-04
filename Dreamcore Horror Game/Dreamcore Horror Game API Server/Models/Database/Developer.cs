e48f34681d1e51f1627a9d137482f2884ced5d2d
KroN136 <kron2002a@yandex.ru>
2024-04-04 19:24:33 +0300

    Updated .NET version and database context

---
+++ Dreamcore Horror Game/Dreamcore Horror Game API Server/Models/Database/Developer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dreamcore_Horror_Game_API_Server.Models.Database;
+
+public partial class Developer
+{
+    public Guid Id { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public Guid? DeveloperAccessLevelId { get; set; }
+
+    public virtual DeveloperAccessLevel? DeveloperAccessLevel { get; set; }
+}
